93f461bbf891ced93b022c8506007f5ff66ac4a1
Gabriel Mak <pixelbreaker@gmail.com>
2015-10-17 09:31:45 +0100

    tweaks

--- cask-installer/install.sh
+++ cask-installer/install.sh
@@ -5,7 +5,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update
 brew install caskroom/cask/brew-cask
 brew tap Homebrew/bundle
+brew install tree
 
-echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.bash_profile
+# echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.bash_profile
 
 brew bundle
