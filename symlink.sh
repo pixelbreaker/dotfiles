01b942c16d5b8fb18614f37cf31f07456ead3b0d
Gabes Mak <gabriel@pixelbreaker.com>
2018-09-18 18:24:31 +0100

    clean up

--- symlink.sh
+++ symlink.sh
@@ -1,8 +1,5 @@
-# Load the shell dotfiles, and then some:
-# * ~/.path can be used to extend `$PATH`.
-# * ~/.extra can be used for other settings you don’t want to commit.
 pwd=$(pwd)
-for file in ~/.{bash_profile,bash_prompt,exports,aliases,functions,extra}; do
+for file in ~/.{bashrc,bash_prompt,exports,aliases,functions,extra}; do
 	#[ -r "$file" ] && [ -f "$file" ] && source "$file";
   filename=${file##*/}
   echo "$pwd/$filename"
--- youtube-dl-scheduler/download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DATE=`date +%Y-%m-%d`
-cat downloads.txt | xargs youtube-dl -o '~/video/youtube-dl/%(title)s-%(id)s.%(ext)s'
-
-mv downloads.txt "downloads-$DATE.txt"
-touch downloads.txt
